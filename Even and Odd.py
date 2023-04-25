a2828aab4b6df7147f872a392fb09911bc9fc23b
unknown <templanzatintin@gmail.com>
2023-04-26 01:01:10 +0800

    First commit: greetings

---
+++ Even and Odd.py
@@ -0,0 +1,9 @@
+# Templanza, Kristine Joy F.
+# BSCPE 1-4
+# Asignnment no. 3 - Even and Odd numbers
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 35)
+print("")
+print("\033[45m ♥ Welcome! ♥ \033[0m".center(78))
