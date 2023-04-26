91728b4fe2e61302834bc62b604a30491bf5e2c2
unknown <templanzatintin@gmail.com>
2023-04-26 22:32:22 +0800

    changed the import random because of error and now the def process is working

--- Even and Odd.py
+++ Even and Odd.py
@@ -8,8 +8,14 @@ print("\033[35m※ \033[0m" * 35)
 print("")
 print("\033[45m ♥ Welcome! ♥ \033[0m".center(78))
 
-import random
-even = []
-odd = []
-
 # open numbers.txt (read), even.txt (write/append), odd.txt (write/append)
+def process():
+    with open("numbers.txt") as input_number_file, open("even.txt", "w") as output_even_file, open("odd.txt", "w") as output_odd_file:
+        for line in input_number_file:
+            integers = int(line.strip())
+            
+            if integers % 2 == 0:
+                output_even_file.write(str(integers) + "\n")
+            else:
+                output_odd_file.write(str(integers) + "\n")
+process()
