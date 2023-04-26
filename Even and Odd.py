0910857f71d7bf0f99f37a3388ebc84b46634851
unknown <templanzatintin@gmail.com>
2023-04-26 22:36:08 +0800

    Pseudocode

--- Even and Odd.py
+++ Even and Odd.py
@@ -11,11 +11,14 @@ print("\033[45m ♥ Welcome! ♥ \033[0m".center(78))
 # open numbers.txt (read), even.txt (write/append), odd.txt (write/append)
 def process():
     with open("numbers.txt") as input_number_file, open("even.txt", "w") as output_even_file, open("odd.txt", "w") as output_odd_file:
+        # read the numbers.txt
         for line in input_number_file:
             integers = int(line.strip())
-            
+            # check the integers
             if integers % 2 == 0:
+                # if even, write/append it in even.txt file
                 output_even_file.write(str(integers) + "\n")
             else:
+                # if odd, write/append it in odd.txt file
                 output_odd_file.write(str(integers) + "\n")
 process()
